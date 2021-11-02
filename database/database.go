b2611e6fa26eb8d6c86a0791bce137aeb9fc13d8
Prince Narteh <prinarteh@gmail.com>
2021-11-02 02:47:16 +0000

    Add Database

---
+++ database/database.go
@@ -0,0 +1,31 @@
+package database
+
+import (
+	"log"
+	"os"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type DBInstance struct {
+	Db *gorm.DB
+}
+
+var Database DBInstance
+
+func ConnectDB() {
+	db, err := gorm.Open(sqlite.Open("api.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to the database\n", err.Error())
+		os.Exit(2)
+	}
+
+	log.Println("Connected to the database successfully")
+	db.Logger = logger.Default.LogMode(logger.Info)
+	log.Println("Running Migrations")
+	// TODO: Add Migrations
+
+	Database = DBInstance{Db: db}
+}
